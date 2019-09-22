861d9da1b56d18e76aa85ba25fd3042cc494a42a
Ranvijay Singh <51271858+ranvijay2012@users.noreply.github.com>
2019-09-22 19:10:06 +0530

    path-variable

--- src/main/java/com/neeraj/rest/webservices/restfulwebservices/HelloWordController.java
+++ src/main/java/com/neeraj/rest/webservices/restfulwebservices/HelloWordController.java
@@ -1,19 +1,28 @@
 package com.neeraj.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWordController {
 
+	// using String return
 	@GetMapping(path = "/hello-world")
 	public String helloWord() {
 		return "Hello World";
 	}
 
+	// using Bean return
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWordBean helloWordBean() {
 		return new HelloWordBean("Hello World");
+	}
+
+	// using Path vaiable return
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWordBean helloWordPathVariable(@PathVariable String name) {
+		return new HelloWordBean(String.format("Hello World, %s", name));
 	}
 
 }
