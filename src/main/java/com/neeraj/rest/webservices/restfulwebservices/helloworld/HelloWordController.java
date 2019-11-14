ecd79707ebda88e41dec378db8a347621bffd001
Ranvijay Singh <51271858+ranvijay2012@users.noreply.github.com>
2019-11-14 23:29:06 +0530

    swagger
    swagger

--- src/main/java/com/neeraj/rest/webservices/restfulwebservices/helloworld/HelloWordController.java
+++ src/main/java/com/neeraj/rest/webservices/restfulwebservices/helloworld/HelloWordController.java
@@ -4,12 +4,10 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.LocaleResolver;
 
 @RestController
 public class HelloWordController {
